3d3bfe692bb545775002b19fb2d06f85fcd82c48
aaerniestudent <aaernie7528@mail.kvcc.edu>
2015-09-21 15:28:35 -0400

    Creates working addition and subtractions buttons with a text view showing the counter. Toasts "Over twenty!" on click of addition at 20.

--- app/src/main/java/edu/kvcc/cis298/cis298assignment1/CounterActivity.java
+++ app/src/main/java/edu/kvcc/cis298/cis298assignment1/CounterActivity.java
@@ -4,15 +4,56 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+import android.widget.Toast;
+
+
 
 public class CounterActivity extends AppCompatActivity {
+
+    private Button mPlusButton;
+    private Button mMinusButton;
+    private TextView mTextView;
+    int count = 0;
+
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_counter);
-    }
+        mTextView = (TextView) findViewById (R.id.text_view);
 
+        mPlusButton = (Button) findViewById(R.id.plus_button);
+        mPlusButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                if (count==20){
+                    Toast.makeText(CounterActivity.this,
+                            R.string.twenty_toast,
+                            Toast.LENGTH_SHORT).show();
+                }
+                count++;
+                String theResult = Integer.toString(count);
+                mTextView.setText(theResult);
+
+            }
+        });
+
+        mMinusButton = (Button) findViewById(R.id.minus_button);
+        mMinusButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                count--;
+                String theResult = Integer.toString(count);
+                mTextView.setText(theResult);
+
+            }
+        });
+
+    }
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
         // Inflate the menu; this adds items to the action bar if it is present.
